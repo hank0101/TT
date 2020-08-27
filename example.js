5191b83086042aeba49ca477de21df95b4fbb614
hank0101 <sampsonkuo@gmail.com>
2020-08-27 18:58:45 +0800

    Add files via upload
    revise rule

---
+++ example.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $('#get-data').click(function () {
+    var showData = $('#show-data');
+
+    $.getJSON('example.json', function (data) {
+      console.log(data);
+
+      var items = data.CRAWL_RESULT.map(function (item) {
+        return item.key + ': ' + item.value;
+      });
+
+      showData.empty();
+
+      if (items.length) {
+        var content = '<li>' + items.join('</li><li>') + '</li>';
+        var list = $('<ul />').html(content);
+        showData.append(list);
+      }
+    });
+
+    showData.text('Loading the JSON file.');
+  });
+});
